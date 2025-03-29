6888a04c017afd621faa28169fd00dff9ec99e54
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-29 01:46:54 -0500

    applied rule for div/0

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 function calculateDecimal() {
-  const decimal1Element = document.querySelector(
-    "#decimalCalculator #dec1Input"
-  );
-  const decimal2Element = document.querySelector(
-    "#decimalCalculator #dec2Input"
-  );
-  const dropdown = document.querySelector("#decimalCalculator #dropdown");
-  const resultElement = document.querySelector("#decimalCalculator .result");
-  const infoElement = document.querySelector("#decimalCalculator .info");
-  let decimal1 = parseFloat(decimal1Element.value);
-  let decimal2 = parseFloat(decimal2Element.value);
-  let result = 0;
+   const decimal1Element = document.querySelector("#decimalCalculator #dec1Input")
+   const decimal2Element = document.querySelector("#decimalCalculator #dec2Input")
+   const dropdown = document.querySelector("#decimalCalculator #dropdown")
+   const resultElement = document.querySelector("#decimalCalculator .result")
+   const infoElement = document.querySelector("#decimalCalculator .info")
+   let decimal1 = parseFloat(decimal1Element.value)
+   let decimal2 = parseFloat(decimal2Element.value)
+   if (isNaN(decimal2)) decimal2 = 0
+   let result = 0
 
-  if (dropdown.value == "Add") {
-    infoElement.innerHTML = "a + b = ?";
-    result = decimal1 + decimal2;
-  } else if (dropdown.value == "Subtract") {
-    infoElement.innerHTML = "a - b = ?";
-    result = decimal1 - decimal2;
-  } else if (dropdown.value == "Multiply") {
-    infoElement.innerHTML = "a × b = ?";
-    result = decimal1 * decimal2;
-  } else if (dropdown.value == "Divide") {
-    infoElement.innerHTML = "a / b = ?";
-    result = decimal1 / decimal2;
-  } else if (dropdown.value == "Exponent") {
-    infoElement.innerHTML = "aᵇ = ?";
-    result = decimal1 ^ decimal2;
-  } else if (dropdown.value == "Root") {
-    //to-do
-  } else if (dropdown.value == "Logarithm") {
-    //to-do
-  }
+   if (dropdown.value == "Add") {
+      infoElement.innerHTML = "a + b = ?"
+      result = decimal1 + decimal2
+   } else if (dropdown.value == "Subtract") {
+      infoElement.innerHTML = "a - b = ?"
+      result = decimal1 - decimal2
+   } else if (dropdown.value == "Multiply") {
+      infoElement.innerHTML = "a × b = ?"
+      result = decimal1 * decimal2
+   } else if (dropdown.value == "Divide") {
+      infoElement.innerHTML = "a / b = ?"
+      if (decimal2 == 0) {
+         result = "infinity"
+      } else result = decimal1 / decimal2
+   } else if (dropdown.value == "Exponent") {
+      infoElement.innerHTML = "aᵇ = ?"
+      result = decimal1 ^ decimal2
+   } else if (dropdown.value == "Root") {
+      //to-do
+   } else if (dropdown.value == "Logarithm") {
+      //to-do
+   }
 
-  resultElement.innerHTML = `Answer: ${result}`;
+   resultElement.innerHTML = `Answer: ${result}`
 }
