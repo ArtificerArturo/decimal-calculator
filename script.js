6cdeea901e8c6fb802d73578e795346b9d72584c
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-29 01:36:20 -0500

    Added info text

--- script.js
+++ script.js
@@ -3,26 +3,30 @@ function calculateDecimal() {
     const decimal2Element = document.querySelector("#decimalCalculator #dec2Input");
     const dropdown = document.querySelector("#decimalCalculator #dropdown")
     const resultElement = document.querySelector("#decimalCalculator .result");
+    const infoElement = document.querySelector("#decimalCalculator .info")
     let decimal1 = parseFloat(decimal1Element.value)
     let decimal2 = parseFloat(decimal2Element.value)
     let result=0
-    console.log(decimal1)
-
 
     if (dropdown.value=="Add") {
+        infoElement.innerHTML = "a + b = ?"
         result = decimal1+decimal2
     } else if (dropdown.value=="Subtract") {
+        infoElement.innerHTML = "a - b = ?"
         result = decimal1-decimal2
     } else if (dropdown.value=="Multiply") {
+        infoElement.innerHTML = "a × b = ?"
         result = decimal1*decimal2
     } else if (dropdown.value=="Divide") {
+        infoElement.innerHTML = "a / b = ?"
         result = decimal1/decimal2
     } else if (dropdown.value=="Exponent") {
+        infoElement.innerHTML = "aᵇ = ?" 
         result = decimal1^decimal2
     } else if (dropdown.value=="Root") {
-
+        //to-do
     } else if (dropdown.value=="Logarithm") {
-
+        //to-do
     }
 
     resultElement.innerHTML = `Answer: ${result}`
